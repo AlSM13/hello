d80f82235dab6dfb0254d5b49587cc987e0e1cdc
Aleksandr <Dr.AleksandrSm@gmail.com>
2022-06-19 15:56:58 +0300

    написали программу

--- Program.cs
+++ Program.cs
@@ -1,5 +1,28 @@
-﻿using System
+﻿using System;
+
 namespace Program
 {
-    class Program
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			String[] strings = {"Qwerty", "man", "Dodo", "Petya", "A", "Windows", "Ara", "Lol", "No", "M"};
+			String[] result = new String[strings.Length];
+
+			int index = 0;
+			foreach (String s in strings)
+            {
+				if (s.Length <= 3)
+                {
+					result[index++] = s;
+				}
+            }
+
+			Console.WriteLine("Result:");
+			for (int i = 0; i < index; i++)
+            {
+				Console.Write(result[i] + " ");
+			}	
+		}
+	}
 }
